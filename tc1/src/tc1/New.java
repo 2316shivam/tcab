ea4b3ccb9887ac5ef43446bc6002f9d9c1d93b46
sdac <sdac@sdac-PC>
2023-04-05 00:33:39 -0700

    end

--- tc1/src/tc1/New.java
+++ tc1/src/tc1/New.java
@@ -4,6 +4,7 @@ public class New {
 	public static void main(String[] args) {
 		System.out.println("version nothing");
 		System.err.println("version something");
+		System.err.println("version end");
 	}
 
 }
